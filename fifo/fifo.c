32ab6a5afc56db9eccbc809dc32eecb642ce8ee3
Thiago Naves <naves.thiago@gmail.com>
2016-10-29 22:54:58 -0200

    Mutex substituído por região crítica para permitir uso com IRQs. Região crítica permite locks mais rápidos que mutex. Funções tipo S substituídas por tipo I. Adicionados testes das funções I.

--- fifo/fifo.c
+++ fifo/fifo.c
@@ -11,6 +11,10 @@
 #define MSG_OK 1
 #define chSysUnlockFromISR()
 #define chSysLockFromISR()
+#define chSysLock()
+#define chSysUnlock()
+#define chDbgCheck(...)
+#define chDbgCheckClassI()
 
 void chSemObjectInit(semaphore_t *s, int val) {
   *s = val;
@@ -50,7 +54,6 @@ void fifoInit(fifo_t * fifo, fifo_item_t * data, size_t size) {
   fifo->read = 0;
   fifo->first_handle = 0;
   fifo->last_handle = 0;
-  chMtxObjectInit(&fifo->mutex);
   chSemObjectInit(&fifo->avaliable, fifo->size);
   chSemObjectInit(&fifo->used, 0);
 }
@@ -63,70 +66,73 @@ static inline void inc_handle(fifo_handle_t *h) {
 }
 
 fifo_handle_t fifoPushWithHandle(fifo_t * fifo, fifo_item_t * data, systime_t timeout) {
-  if (!fifo) {
-    return HANDLE_INVALID;
-  }
+  chDbgCheck(fifo != NULL);
 
-  if (chSemWaitTimeout(&fifo->avaliable, timeout) != MSG_OK) {
+  if (chSemWaitTimeout(&fifo->avaliable, timeout) != MSG_OK)
     return HANDLE_INVALID;
-  }
 
   fifo_handle_t ret;
-  chMtxLock(&fifo->mutex);
+  chSysLock();
   ret = fifo->last_handle;
 
   // Create a new handle for the next push
   inc_handle(&fifo->last_handle);
 
-  // TODO mover o memcpy para fora do mutex. semaforo used garante que os dados nao serao sobrescritos
-  // TODO regiao critica para concorrencia com IRQ
-  memcpy(&fifo->data[fifo->write], data, sizeof(fifo_item_t));
-  fifo->data[fifo->write].status = CB_FIFO_QUEUED;
+  fifo_item_t * q_data = &fifo->data[fifo->write];
   fifo->write = (fifo->write + 1) % fifo->size;
-  chMtxUnlock(&fifo->mutex);
+  chSysUnlock();
+
+  memcpy(q_data, data, sizeof(fifo_item_t));
+  q_data->status = CB_FIFO_QUEUED;
+
+  // Allow pop only after copying the data
   chSemSignal(&fifo->used);
   return ret;
 }
 
 bool fifoPop(fifo_t * fifo, fifo_item_t * data, systime_t timeout) {
-  if (!fifo) {
-    return false;
-  }
+  chDbgCheck(fifo != NULL);
 
-  if (chSemWaitTimeout(&fifo->used, timeout) != MSG_OK) {
+  if (chSemWaitTimeout(&fifo->used, timeout) != MSG_OK)
     return false;
-  }
 
-  // TODO mover o memcpy para fora do mutex. semaforo avaliable garante que os dados nao serao sobrescritos
-  // TODO regiao critica para concorrencia com IRQ
-  chMtxLock(&fifo->mutex);
-  if (data) {
-    memcpy(data, &fifo->data[fifo->read], sizeof(fifo_item_t));
-  }
+  fifo_item_t * q_data;
+
+  chSysLock();
+  q_data = &fifo->data[fifo->read];
   fifo->read = (fifo->read + 1) % fifo->size;
 
   // Invalidade the read element handle
   inc_handle(&fifo->first_handle);
 
-  chMtxUnlock(&fifo->mutex);
-  chSemSignal(&fifo->avaliable);
+  chSysUnlock();
 
-  if ((data->callback) && (data->status != CB_FIFO_DELETED))
-    data->callback(data);
+  if (q_data->status != CB_FIFO_DELETED) {
+    if (data)
+      memcpy(data, q_data, sizeof(fifo_item_t));
+
+    if (q_data->callback)
+      q_data->callback(q_data);
+  }
+  else {
+    // Item is deleted, make the position avaliable and return failure
+    chSemSignal(&fifo->avaliable);
+    return false;
+  }
+
+  // Make the fifo position avaliable only after copying the data
+  chSemSignal(&fifo->avaliable);
 
   return true;
 }
 
 fifo_handle_t fifoPushWithHandleI(fifo_t * fifo, fifo_item_t * data) {
-  if (!fifo) {
-    return HANDLE_INVALID;
-  }
+  chDbgCheckClassI();
+  chDbgCheck(fifo != NULL);
+  chDbgCheck(data != NULL);
 
-//  chSysLockFromISR();
-  if (chSemGetCounterI(&fifo->avaliable) == 0) {
-//    chSysUnlockFromISR();
+  if (chSemGetCounterI(&fifo->avaliable) == 0)
     return HANDLE_INVALID;
-  }
 
   // Create a new handle for the next push
   fifo_handle_t ret = fifo->last_handle;
@@ -137,62 +143,71 @@ fifo_handle_t fifoPushWithHandleI(fifo_t * fifo, fifo_item_t * data) {
   fifo->data[fifo->write].status = CB_FIFO_QUEUED;
   fifo->write = (fifo->write + 1) % fifo->size;
   chSemSignalI(&fifo->used);
-//  chSysUnlockFromISR();
   return ret;
 }
 
 bool fifoPopI(fifo_t * fifo, fifo_item_t * data) {
-  if (!fifo) {
-    return false;
-  }
+  chDbgCheckClassI();
+  chDbgCheck(fifo != NULL);
 
-//  chSysLockFromISR();
-  if (chSemGetCounterI(&fifo->used) == 0) {
-//    chSysUnlockFromISR();
+  if (chSemGetCounterI(&fifo->used) == 0)
     return false;
-  }
+
+  chSemFastWaitI(&fifo->used);
 
   // Invalidade the read element handle
   inc_handle(&fifo->first_handle);
 
-  if (data) {
-    memcpy(data, &fifo->data[fifo->read], sizeof(fifo_item_t));
-  }
+  fifo_item_t * q_data = &fifo->data[fifo->read];
   fifo->read = (fifo->read + 1) % fifo->size;
+
+  if (q_data->status != CB_FIFO_DELETED) {
+    if (data)
+      memcpy(data, q_data, sizeof(fifo_item_t));
+
+    if (q_data->callback)
+      q_data->callback(q_data);
+  }
+  else {
+    chSemSignalI(&fifo->avaliable);
+    return false;
+  }
+
   chSemSignalI(&fifo->avaliable);
-//  chSysUnlockFromISR();
-
-  if ((data->callback) && (data->status != CB_FIFO_DELETED))
-    data->callback(data);
-
   return true;
 }
 
 bool fifoDelete(fifo_t * fifo, fifo_handle_t handle) {
+  chDbgCheck(fifo != NULL);
+
   if (handle == HANDLE_INVALID)
     return false;
 
-  chMtxLock(&fifo->mutex);
+  chSysLock();
   fifo_item_t * elem = NULL;
   size_t index = handle % fifo->size;
 
   if (fifo->first_handle > fifo->last_handle) {
-    if ((handle >= fifo->first_handle) || ((handle >= 0) && (handle < fifo->last_handle)))
+    if ((handle >= fifo->first_handle) || ((handle >= 0) && (handle < fifo->last_handle))) {
       elem = &fifo->data[index];
+      elem->status = CB_FIFO_DELETED;
+    }
   }
   else {
-    if ((handle >= fifo->first_handle) && (handle < fifo->last_handle))
+    if ((handle >= fifo->first_handle) && (handle < fifo->last_handle)) {
       elem = &fifo->data[index];
+      elem->status = CB_FIFO_DELETED;
+    }
   }
-  chMtxUnlock(&fifo->mutex);
 
   // DEBUG
   if (elem) {
-    elem->status = CB_FIFO_DELETED;
     printf("Delete %d: %d\n", handle, (int)elem->data);
   }
   else
     printf("Delete %d: Invalido\n", handle);
+
+  chSysUnlock();
 
   return elem != NULL;
 }
--- fifo/teste.c
+++ fifo/teste.c
@@ -24,17 +24,47 @@ bool pop() {
   if (fifoPop(&fifo, &i, 0)) {
     if (i.status != CB_FIFO_DELETED)
       printf("Pop: %d\n", (int)i.data);
-    else
+    else // Should not happen
       printf("Pop: %d (deleted)\n", (int)i.data);
 
     return true;
   }
+  else
+    printf("Pop failed\n");
 
   return false;
 }
 
+int pushI(char * d) {
+  fifo_item_t i;
+  i.data = d;
+  i.data_size = 1;
+  i.callback_data = 0;
+  i.callback =  callback;
+  return fifoPushWithHandleI(&fifo, &i);
+}
+
+bool popI() {
+  fifo_item_t i;
+  if (fifoPopI(&fifo, &i)) {
+    if (i.status != CB_FIFO_DELETED)
+      printf("PopI: %d\n", (int)i.data);
+    else // Should not happen
+      printf("PopI: %d (deleted)\n", (int)i.data);
+
+    return true;
+  }
+  else
+    printf("PopI failed\n");
+
+  return false;
+}
+
+
 int main() {
   fifoInit(&fifo, data, sizeof(data));
+
+  printf("Thread functions tests\n\n");
 
   for (int i=0; i<6; i++) {
     int id = push((char *)i);
@@ -46,7 +76,8 @@ int main() {
   fifoDelete(&fifo, 2);
   //printf("Delete ID: 2\n");
 
-  while(pop());
+  for (int i=0; i<6; i++)
+    pop();
 
   for (int i=0; i<6; i++) {
     int id = push((char *)(i+10));
@@ -58,6 +89,38 @@ int main() {
   fifoDelete(&fifo, 4);
   fifoDelete(&fifo, 10);
   fifoDelete(&fifo, 7);
+
+  printf("\n-----------------------------\n");
+  printf("IRQ functions tests\n\n");
+
+  fifoInit(&fifo, data, sizeof(data));
+
+  for (int i=0; i<6; i++) {
+    int id = pushI((char *)i);
+    printf("PushI %d - ID: %d\n", i, id);
+  }
+
+  popI();
+
+  fifoDelete(&fifo, 2);
+  //printf("Delete ID: 2\n");
+
+  for (int i=0; i<5; i++)
+    popI();
+
+  popI();
+
+  for (int i=0; i<6; i++) {
+    int id = pushI((char *)(i+10));
+    printf("PushI %d - ID: %d\n", i+10, id);
+  }
+
+  fifoDelete(&fifo, -1);
+  fifoDelete(&fifo, 1);
+  fifoDelete(&fifo, 4);
+  fifoDelete(&fifo, 10);
+  fifoDelete(&fifo, 7);
+
 
   return 0;
 }
